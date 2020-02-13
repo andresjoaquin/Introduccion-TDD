3e644c6b6aa40b41bf870b0ba93a35995f1f3ba1
andresjoaquin <andresjoaquin@gmail.com>
2020-02-13 13:17:51 -0300

    Se agrega IntroTdd.Web

---
+++ IntroTdd/IntroTdd.Web/IntroTdd.Web/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using IntroTdd.Web.Models;
+
+namespace IntroTdd.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new Cuenta());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Depositar(Cuenta cuenta)
+        {
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+                cuenta.Saldo += cuenta.Monto;
+                cuenta.Monto = 0;
+            }
+
+            return View("Index",cuenta);
+        }
+    }
+}
---
+++ IntroTdd/IntroTdd.Web/IntroTdd.Web/Models/CuentaModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroTdd.Web.Models
+{
+    public class Cuenta
+    {
+        public int Monto { get; set; }
+        public int Saldo { get; set; }
+
+        public Cuenta()
+        {
+            Monto = 0;
+            Saldo = 0;
+        }
+    }
+}
